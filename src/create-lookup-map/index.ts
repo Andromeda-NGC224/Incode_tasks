5db2eb726c108a14335d8fecd65bbb912a116c04
work <shadim115115@gmail.com>
2025-05-20 14:25:07 +0300

    done task create-lookup-map

--- src/create-lookup-map/index.ts
+++ src/create-lookup-map/index.ts
@@ -17,5 +17,36 @@ import { ItemWithId } from './types';
 export const createLookupMap = <T extends ItemWithId>(
   array: T[],
 ): Record<number, T> => {
-  throw new Error('Not Implemented');
+  const map: Record<number, T> = {};
+
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    map[item.id] = item;
+  }
+
+  return map;
 };
+createLookupMap([
+  { id: 1, value: 'a' },
+  { id: 1, value: 'override' },
+  { id: 2, value: null },
+  { id: 3, nested: { key: 'val' } },
+]);
+
+export const createLookupMap2 = <T extends ItemWithId>(
+  array: T[],
+): Record<number, T> => {
+  const map: Record<number, T> = {};
+
+  array.forEach((item) => {
+    map[item.id] = item;
+  });
+
+  return map;
+};
+createLookupMap2([
+  { id: 1, value: 'a' },
+  { id: 1, value: 'override' },
+  { id: 2, value: null },
+  { id: 3, nested: { key: 'val' } },
+]);
