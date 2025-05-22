d26c63fd0c25e2049a64d3972deac77fb22430b5
Andromeda-NGC224 <85696963+Andromeda-NGC224@users.noreply.github.com>
2025-05-22 12:21:03 +0300

    feat(group-by-property): implement object array, that grouping by groupName (#9)

--- src/group-by-property/index.test.ts
+++ src/group-by-property/index.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest';
 import * as functions from './index';
 import { groupByCases } from './mock';
 
-describe('groupBy', () => {
+describe('groupByProperty', () => {
   groupByCases.forEach(({ input, key, expected }, index) => {
     Object.entries(functions).forEach(([fnName, fn]) => {
       it(`function: ${fnName}, mock data index: ${index}`, () => {
